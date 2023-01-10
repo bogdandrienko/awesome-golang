d6096581cf86773157aebb8e7a388cce029947a2
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2023-01-10 22:21:18 +0600

    update

--- 12_context.go
+++ 12_context.go
@@ -6,13 +6,21 @@ import (
 	"time"
 )
 
+// context.Background - on the top level
+// context.TODO - unknown task
+// context.Value - use small and set not needed values
+
 func context1() {
 	ctx := context.Background()
-	ctx, _ = context.WithTimeout(ctx, time.Second*1)
-	parse(ctx)
+	ctx, cancel := context.WithTimeout(ctx, time.Second*1)
+	go func() {
+		time.Sleep(time.Millisecond * 100)
+		cancel()
+	}()
+	parse1(ctx)
 }
 
-func parse(ctx context.Context) {
+func parse1(ctx context.Context) {
 	for {
 		select {
 		case <-time.After(time.Second * 2):
@@ -21,10 +29,38 @@ func parse(ctx context.Context) {
 		case <-ctx.Done():
 			fmt.Println("deadline exceded")
 			return
+		default:
+
+		}
+	}
+}
+
+func context2() {
+	ctx := context.Background()
+	ctx, _ = context.WithTimeout(ctx, time.Second*3)
+	ctx = context.WithValue(ctx, "id", 1)
+
+	parse2(ctx)
+}
+
+func parse2(ctx context.Context) {
+	id := ctx.Value("id")
+	fmt.Println(id.(int))
+	for {
+		select {
+		case <-time.After(time.Second * 2):
+			fmt.Println("parsing completed")
+			return
+		case <-ctx.Done():
+			fmt.Println("deadline exceded")
+			return
+		default:
+
 		}
 	}
 }
 
 func main() {
-	context1()
+	//context1()
+	context2()
 }
